cf16c8b2f849df43e93d92b211fbd096a4971e77
Mingjia Yu <mingjiayu99@gmail.com>
2025-03-22 14:06:59 -0700

    fix frontend CORS issue

---
+++ frontend/react/src/App.jsx
@@ -0,0 +1,54 @@
+import { Wrap, WrapItem, Spinner, Text } from "@chakra-ui/react";
+import SidebarWithHeader from "./components/shared/SideBar.jsx";
+import { useEffect, useState } from "react";
+import { getCustomers } from "./services/client.jsx";
+import CardWithImage from "./components/Card.jsx";
+
+const App = () => {
+  const [customers, setCustomers] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    getCustomers()
+      .then((res) => {
+        setCustomers(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return (
+      <SidebarWithHeader>
+        <Spinner />
+      </SidebarWithHeader>
+    );
+  }
+
+  if (customers.length <= 0) {
+    return (
+      <SidebarWithHeader>
+        <Text>No customers available</Text>
+      </SidebarWithHeader>
+    );
+  }
+
+  return (
+    <SidebarWithHeader>
+      <Wrap justify={"center"} spacing={"30"}>
+        {customers.map((customer, index) => (
+          <WrapItem key={index}>
+            <CardWithImage {...customer} />
+          </WrapItem>
+        ))}
+      </Wrap>
+    </SidebarWithHeader>
+  );
+};
+
+export default App;
---
+++ frontend/react/src/theme.js
@@ -0,0 +1,8 @@
+// src/theme.js
+import { theme as baseTheme } from '@chakra-ui/react';
+
+const theme = {
+  ...baseTheme
+};
+
+export default theme;
